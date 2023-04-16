3417eb4e5624deb315166fd6c1d4bc042ae75480
tucani <130901429+tucani@users.noreply.github.com>
2023-04-16 22:11:58 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("test7");
+		System.out.println("test8");
 	}
 
 }
